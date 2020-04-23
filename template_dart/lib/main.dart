b47ac688ca63585fdd260af8c5e2b8ba7dda410e
hxingood <497046388@qq.com>
2020-04-23 11:17:54 +0800

    新增flutter页面跳转native页面返回数据例子

--- template_dart/lib/main.dart
+++ template_dart/lib/main.dart
@@ -3,6 +3,27 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
