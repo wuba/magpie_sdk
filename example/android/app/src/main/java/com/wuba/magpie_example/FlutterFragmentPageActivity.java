1c35fcbb844e34d5b5e59425b78ef807462fee3a
黄鑫 <huangxin01@58.com>
2020-06-09 17:20:17 +0800

    androidx版本

--- example/android/app/src/main/java/com/wuba/magpie_example/FlutterFragmentPageActivity.java
+++ example/android/app/src/main/java/com/wuba/magpie_example/FlutterFragmentPageActivity.java
@@ -7,13 +7,14 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.idlefish.flutterboost.containers.FlutterFragment;
 
--- example/android/app/src/main/java/com/wuba/magpie_example/MainActivity.java
+++ example/android/app/src/main/java/com/wuba/magpie_example/MainActivity.java
@@ -2,10 +2,12 @@ package com.wuba.magpie_example;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.alibaba.fastjson.JSON;
 import com.wuba.magpie.Magpie;
